c450d47ac91ae9d7a0af76526511fe8ac591331e
Imane Wakrim <110701275+imanewkm@users.noreply.github.com>
2024-02-20 22:56:48 +0100

    Update 102-counting_sort.c

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -11,7 +11,7 @@
 */
 void counting_sort(int *array, size_t size)
 {
-	int i, max;
+	int i, maxim;
 	int *counter = NULL, *copy = NULL;
 	size_t j, temp, total = 0;
 
@@ -21,23 +21,23 @@ void counting_sort(int *array, size_t size)
 	copy = malloc(sizeof(int) * size);
 	if (copy == NULL)
 		return;
-	for (j = 0, max = 0; j < size; j++)
+	for (j = 0, maxim = 0; j < size; j++)
 	{
 		copy[j] = array[j];
-		if (array[j] > max)
+		if (array[j] > maxim)
 			max = array[j];
 	}
-	counter = malloc(sizeof(int) * (max + 1));
+	counter = malloc(sizeof(int) * (maxim + 1));
 	if (counter == NULL)
 	{
 		free(copy);
 		return;
 	}
-	for (i = 0; i <= max; i++)
+	for (i = 0; i <= maxim; i++)
 		counter[i] = 0;
 	for (j = 0; j < size; j++)
 		counter[array[j]] += 1;
-	for (i = 0; i <= max; i++)
+	for (i = 0; i <= maxim; i++)
 	{
 		temp = counter[i];
 		counter[i] = total;
@@ -48,7 +48,7 @@ void counting_sort(int *array, size_t size)
 		array[counter[copy[j]]] = copy[j];
 		counter[copy[j]] += 1;
 	}
-	print_array(counter, max + 1);
+	print_array(counter, maxim + 1);
 	free(counter);
 	free(copy);
 }
