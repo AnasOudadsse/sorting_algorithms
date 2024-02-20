b3dc5e88d8862c768d675e1b4f7b352063ce6bf9
Anas Oudadsse <anas.oudadsse1@gmail.com>
2024-02-20 23:32:13 +0100

    105

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -18,45 +18,45 @@ unsigned int tenPower(unsigned int pow)
 }
 
 /**
-* count_sort - sorts an array of integers in ascending order at a specific
+* sortCount - sorts an array of integers in ascending order at a specific
 * digit location using the Counting sort algorithm
-* @array: array to sort
-* @size: size of the array to sort
-* @digit: digit to sort by
+* @arr: arr to sort
+* @size: size of the arr to sort
+* @integerr: integerr to sort by
 *
 * Return: 1 if there is a need to keep sorting, 0 if not
 */
-unsigned int count_sort(int *array, size_t size, unsigned int digit)
+unsigned int sortCount(int *arr, size_t size, unsigned int integerr)
 {
 	int i, count[10] = {0};
 	int *copy = NULL;
-	size_t j, temp, total = 0;
+	size_t j, tmp, result = 0;
 	unsigned int dp1, dp2, sort = 0;
 
-	dp2 = tenPower(digit - 1);
+	dp2 = tenPower(integerr - 1);
 	dp1 = dp2 * 10;
 	copy = malloc(sizeof(int) * size);
 	if (copy == NULL)
 		exit(1);
 	for (j = 0; j < size; j++)
 	{
-		copy[j] = array[j];
-		if (array[j] / dp1 != 0)
+		copy[j] = arr[j];
+		if (arr[j] / dp1 != 0)
 			sort = 1;
 	}
 	for (i = 0; i < 10 ; i++)
 		count[i] = 0;
 	for (j = 0; j < size; j++)
-		count[(array[j] % dp1) / dp2] += 1;
+		count[(arr[j] % dp1) / dp2] += 1;
 	for (i = 0; i < 10; i++)
 	{
-		temp = count[i];
-		count[i] = total;
-		total += temp;
+		tmp = count[i];
+		count[i] = result;
+		result += tmp;
 	}
 	for (j = 0; j < size; j++)
 	{
-		array[count[(copy[j] % dp1) / dp2]] = copy[j];
+		arr[count[(copy[j] % dp1) / dp2]] = copy[j];
 		count[(copy[j] % dp1) / dp2] += 1;
 	}
 	free(copy);
@@ -79,7 +79,7 @@ void radix_sort(int *array, size_t size)
 		return;
 	for (i = 1; sort == 1; i++)
 	{
-		sort = count_sort(array, size, i);
+		sort = sortCount(array, size, i);
 		print_array(array, size);
 	}
 }
