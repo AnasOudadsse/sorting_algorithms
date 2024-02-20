a0ec6f79ec7e2508f28f2ef572d193f49a0c8ca2
Anas Oudadsse <anas.oudadsse1@gmail.com>
2024-02-20 22:20:09 +0100

    file 100

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -10,25 +10,25 @@ void insertionSort(int *, long int, long int, size_t);
 */
 void shell_sort(int *array, size_t size)
 {
-	long int n = 1;
-	long int i, k, temp;
+	long int num = 1;
+	long int j, ko, tmp;
 
 	if (size < 2)
 		return;
 
-	while (n < (long int)size / 3)
-		n = n * 3 + 1;
+	while (num < (long int)size / 3)
+		num = num * 3 + 1;
 
-	while (n > 0)
+	while (num > 0)
 	{
-		for (i = n; i < (long int) size; i++)
+		for (j = num; j < (long int) size; j++)
 		{
-			temp = array[i];
-			for (k = i; k >= n && array[k - n] > temp; k = k - n)
-				array[k] = array[k - n];
-			array[k] = temp;
+			tmp = array[j];
+			for (ko = j; ko >= num && array[ko - num] > tmp; ko = ko - num)
+				array[ko] = array[ko - num];
+			array[ko] = tmp;
 		}
 		print_array(array, size);
-		n = (n - 1) / 3;
+		num = (num - 1) / 3;
 	}
 }
