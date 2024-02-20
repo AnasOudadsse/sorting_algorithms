bf208dc5103e614e83bcf66f079e39adfcd82a9e
Imane Wakrim <110701275+imanewkm@users.noreply.github.com>
2024-02-21 00:25:24 +0100

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,6 +1,29 @@
 #include "sort.h"
 
-void swap_list(listint_t *, listint_t *, listint_t **);
+/**
+ * swap_list - swaps two members of a list
+ *
+ * @curr: current node
+ * @prev: previous node
+ * @h: h of list
+ */
+void listSwap(listint_t *curr, listint_t *prev, listint_t **h)
+{
+    listint_t *tmp1 = curr->next;
+    listint_t *tmp2 = prev->prev;
+
+    if (tmp1 != NULL)
+        tmp1->prev = prev;
+    if (tmp2 != NULL)
+        tmp2->next = curr;
+    curr->prev = tmp2;
+    prev->next = tmp1;
+    curr->next = prev;
+    prev->prev = curr;
+    if (*h == prev)
+        *h = curr;
+    print_list(*h);
+}
 
 /**
  * insertion_sort_list - sorts a list using insertion sort
@@ -27,29 +50,4 @@ void insertion_sort_list(listint_t **list)
         }
         curr = curr->next;
     }
-}
-
-/**
- * swap_list - swaps two members of a list
- *
- * @curr: current node
- * @prev: previous node
- * @h: h of list
- */
-void swap_list(listint_t *curr, listint_t *prev, listint_t **h)
-{
-    listint_t *tp1 = curr->next;
-    listint_t *tp2 = prev->prev;
-
-    if (tp1 != NULL)
-        tp1->prev = prev;
-    if (tp2 != NULL)
-        tp2->next = curr;
-    curr->prev = tp2;
-    prev->next = tp1;
-    curr->next = prev;
-    prev->prev = curr;
-    if (*h == prev)
-        *h = curr;
-    print_list(*h);
 }
