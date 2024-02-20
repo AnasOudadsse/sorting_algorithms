7ac42ff211fb877d121a9b8de97c5ec7eee6ca51
Imane Wakrim <110701275+imanewkm@users.noreply.github.com>
2024-02-20 22:49:28 +0100

    Update 101-cocktail_sort_list.c

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,6 +1,29 @@
 #include "sort.h"
 
-void listSwap(listint_t *curr, listint_t *prev, listint_t **head);
+/**
+* listSwap - swaps two members of a list
+*
+* @curr: current node
+* @prev: previous node
+* @head: head of list
+*/
+void listSwap(listint_t *curr, listint_t *prev, listint_t **head)
+{
+	listint_t *temp1 = curr->next;
+	listint_t *temp2 = prev->prev;
+
+	if (temp1 != NULL)
+		temp1->prev = prev;
+	if (temp2 != NULL)
+		temp2->next = curr;
+	curr->prev = temp2;
+	prev->next = temp1;
+	curr->next = prev;
+	prev->prev = curr;
+	if (*head == prev)
+		*head = curr;
+	print_list(*head);
+}
 
 /**
 * cocktail_sort_list - Sorts a Doubly linked list with cocktail sort
@@ -34,29 +57,4 @@ void cocktail_sort_list(listint_t **list)
 		}
 		min = cur;
 	} while (min != max);
-}
-
-/**
-* listSwap - swaps two members of a list
-*
-* @curr: current node
-* @prev: previous node
-* @head: head of list
-*/
-void listSwap(listint_t *curr, listint_t *prev, listint_t **head)
-{
-	listint_t *temp1 = curr->next;
-	listint_t *temp2 = prev->prev;
-
-	if (temp1 != NULL)
-		temp1->prev = prev;
-	if (temp2 != NULL)
-		temp2->next = curr;
-	curr->prev = temp2;
-	prev->next = temp1;
-	curr->next = prev;
-	prev->prev = curr;
-	if (*head == prev)
-		*head = curr;
-	print_list(*head);
 }
